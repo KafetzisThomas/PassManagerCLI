97aca0f530b7541a7d2054915c9e64ed647407a0
KafetzisThomas <tomkafetzis06@gmail.com>
2022-10-08 15:13:27 +0300

    Run check on python version, must be 3.6 or higher because of f strings

--- Pass_Manager.py
+++ Pass_Manager.py
@@ -50,6 +50,12 @@ print("Checking for available package updates...")
 os.system("pip install --upgrade -r requirements.txt")
 os.system("cls")
 
+# Run check on python version, must be 3.6 or higher because of f strings
+if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+    print("Error Code U-2: This program requires running python 3.6 or higher! You are running" + str(sys.version_info[0]) + "." + str(sys.version_info[1]))
+    input("Press Enter to exit...")
+    sys.exit()
+
 ###################################### MAIN ##############################################
 
 print(f"""{Fore.LIGHTYELLOW_EX}
