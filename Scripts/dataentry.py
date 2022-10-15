d94068f963a1b544580c71e25864662d294e3984
KafetzisThomas <tomkafetzis06@gmail.com>
2022-10-15 17:42:58 +0300

    'note' row added
    From now it is possible to take notes for every online account you are going to store into the database

--- Scripts/dataentry.py
+++ Scripts/dataentry.py
@@ -3,18 +3,19 @@
 import sqlite3, Scripts.encrypt as encrypt
 
 # Importing encrypted data into the database
-def enter_data(name,username,password,website):
+def enter_data(name,username,password,website,note):
     connection = sqlite3.connect("data.db")
     cursor = connection.cursor()
-    table = """INSERT INTO VAULT(name,username,password,website)
-            VALUES (?,?,?,?);"""
+    table = """INSERT INTO VAULT(name,username,password,website,note)
+            VALUES (?,?,?,?,?);"""
     
     name = encrypt.Encrypt(name)
     username = encrypt.Encrypt(username)
     password = encrypt.Encrypt(password)
     website = encrypt.Encrypt(website)
+    note = encrypt.Encrypt(note)
 
-    combination = (name,username,password,website)
+    combination = (name,username,password,website,note)
     cursor.execute(table,combination)
     connection.commit()
     connection.close()
