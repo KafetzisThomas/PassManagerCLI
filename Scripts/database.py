4f44faeb11a74a127249c1d1231c944908337716
KafetzisThomas <tomkafetzis06@gmail.com>
2024-04-05 23:04:05 +0300

    Encrypt/Decrypt data in db (table: items)

--- Scripts/database.py
+++ Scripts/database.py
@@ -3,7 +3,13 @@
 import sqlite3, sys, getpass, bcrypt
 from Scripts.user import User
 from prettytable import from_db_cursor
+from cryptography.fernet import Fernet
+from prettytable import PrettyTable
 
+#key = Fernet.generate_key()
+
+# ** this key is currently only for testing **
+key = "-n9ISIWpULnLIGwatu-BDrf_Ob1xfhsKOZ_rbRn2KXU="
 
 
 def create_connection():
@@ -14,7 +20,8 @@ def create_connection():
 def create_credentials_table(cur):
         cur.execute("""CREATE TABLE credentials (
                     master_username text,
-                    master_password text
+                    master_password text,
+                    key
                     )""")
 
 def create_items_table(cur):
@@ -29,12 +36,24 @@ def create_items_table(cur):
 
 def insert_data_to_credentials(conn, cur, user):
     with conn:
-        cur.execute("INSERT INTO credentials VALUES (:master_username, :master_password)", {'master_username': user.master_username, 'master_password': user.master_password})
+        cur.execute("INSERT INTO credentials VALUES (:master_username, :master_password, :key)", {'master_username': user.master_username, 'master_password': user.master_password, 'key': key})
+    return key
 
+def encrypt(message, key):
+    return Fernet(key).encrypt(message)
+
+
+def decrypt(token, key):
+    return Fernet(key).decrypt(token)
 
 def insert_data_to_items(conn, cur, item):
+    f = Fernet(key)
+    website = encrypt(item.website.encode(), key).decode('utf-8')
+    username = encrypt(item.username.encode(), key).decode('utf-8')
+    password = encrypt(item.password.encode(), key).decode('utf-8')
+    notes = encrypt(item.notes.encode(), key).decode('utf-8')
     with conn:
-        cur.execute("INSERT INTO items VALUES (:name, :date_posted, :website, :username, :password, :notes)", {'name': item.name, 'date_posted': item.date_posted, 'website': item.website, 'username': item.username, 'password': item.password, 'notes': item.notes})
+        cur.execute("INSERT INTO items VALUES (:name, :date_posted, :website, :username, :password, :notes)", {'name': item.name, 'date_posted': item.date_posted, 'website': website, 'username': username, 'password': password, 'notes': notes})
 
 def get_master_password_by_username(cur, master_username):
     cur.execute("SELECT * FROM credentials WHERE master_username=:master_username", {'master_username': master_username})
@@ -45,12 +64,16 @@ def get_master_password_by_username(cur, master_username):
 
 
 def print_data(cur):
-    cur.execute("SELECT * FROM items")
-    data = cur.fetchall()
-    print(data)
-
     cur.execute("SELECT name, username, password, website, notes, date_posted FROM items")
-    mytable = from_db_cursor(cur)
+    mytable = PrettyTable(["Name", "Username", "Password", "Website", "Notes", "Date Posted"])
+
+    for row in cur.fetchall():
+        website = decrypt(row[3], key).decode()
+        username = decrypt(row[1], key).decode()
+        password = decrypt(row[2], key).decode()
+        notes = decrypt(row[4], key).decode()
+        mytable.add_row([row[0], username, password, website, notes, row[5]])
+
     print(mytable)
 
 
